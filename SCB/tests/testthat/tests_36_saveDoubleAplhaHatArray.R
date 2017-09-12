0eac87b661cfff3fe6ba247f1872d284be057ff3
statistics101 <garbuzov.boris@gmail.com>
2017-09-12 10:01:09 -0400

    almost nothing

--- SCB/tests/testthat/tests_36_saveDoubleAplhaHatArray.R
+++ SCB/tests/testthat/tests_36_saveDoubleAplhaHatArray.R
@@ -3,17 +3,10 @@ saveDoubleAplhaHatArrayFunction <- function()
   cat ("\n Testing \'saveDoubleAplhaHatArray\'\n")
   myTParCount  <- 10
   myTParArray  <- createTParArray(tParCount = myTParCount)
-
-<<<<<<< HEAD
   mySuperReplicationCount <- 10
   myReplicationCount <- 20
   mySampleSize <- 20
-=======
-  mySuperReplicationCount <- 6
-  myReplicationCount <- 7
-  mySampleSize <- 60
 
->>>>>>> fddf7ec612e627e078ceab8ae8c87cac7d73c562
   myLag <- 1
   myLagCount <- 4
   myKernel <- normalDifferenceKernel
