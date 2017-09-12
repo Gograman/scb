28a7768380e2cf5f4acdd24e3fe875aac8a6c6dc
statistics101 <garbuzov.boris@gmail.com>
2017-09-12 14:39:59 +0300

    eliminating lagCount...
    eliminating lagCount as a separate substance in data structures starting allCorHats and on up

---
+++ R/createBandsBrick.R
@@ -0,0 +1,65 @@
+#' @title createBandsArray
+
+#' @name createBandsArray
+#'
+#' @aliases createBands
+#'
+#' @description Create a three-dimensional array of confidence intervals bands of size c(replicationCount, sampleSize, 2).
+#'
+#' @param nonCoverageProbability the probability of non-covering the real values with the estimated ones. \code{alpha = 0.05} by default.
+#'
+#' @return A two dimensional vector representing the lower and the upper bound of CI.
+#'
+#' @examples
+#'mySampleSize=40
+#'myTParCount = 2
+#'mockTParArray <- createTParArray(tParCount = myTParCount)
+# may be different
+#'mockTVMA1Array <- createTVMA1CoefArray(coefFunction = sin,sampleSize = mySampleSize)
+#'myReplicationCount=5
+#'myLag = 1
+#'myLagCount = 4
+#'myKernel = normalDifferenceKernel
+#'myBandwidth = 0.5
+#'myNonCoverageProbability=0.05
+#'bandsBrick=createBandsBrick(tParArray=mockTParArray,
+#'                            lag=myLag,
+#'                            lagCount=myLagCount,
+#'                           bandwidth= myBandwidth,
+#'                           kernel = normalDifferenceKernel,
+#'                            sampleSize=mySampleSize,
+#'                            nonCoverageProbability = myNonCoverageProbability,
+#'                            replicationCount=myReplicationCount)
+#'
+
+createBandsBrick <- function(tParArray,
+                             lag,
+                             bandwidth,
+                             kernel = normalDifferenceKernel,
+                             sampleSize,
+                             nonCoverageProbability = 0.05,
+                             replicationCount)
+{
+  # generate replicationCount of samples
+  # for each sample, create a band
+  # pack those bands together into a brick
+  # return that brick
+  tParCount=length(tParArray)
+  bandsArray <- array(0, dim = c(replicationCount, tParCount, 2))
+
+  for (repCountIndex in 1:replicationCount)
+  {
+
+    bandsArray[repCountIndex, ,] <-
+      createBand(
+        tParArray = tParArray,
+        lag = lag,
+        bandwidth = bandwidth,
+        kernel = kernel,
+        sampleSize = sampleSize,
+        nonCoverageProbability = nonCoverageProbability
+      )
+  }
+
+  bandsArray
+}
---
+++ tests/testthat/tests_32_computeNonCoverageFreqArray.R
@@ -0,0 +1,37 @@
+computeNonCoverageFreqArrayFunction <- function() {
+  cat("\n Testing \'computeNonCoverageFreqArray\' \n")
+
+  myTParCount  <-2
+  myTParArray  <- createTParArray(tParCount = myTParCount)
+  mySuperReplicationCount <- 4
+  myReplicationCount <- 4
+  mySampleSize <- 6
+  myLag <- 1
+  myLagCount <- 4
+  myKernel <- normalDifferenceKernel
+  myBandwidth <- 0.5
+  myNonCoverageProbability <- 0.05
+Start=Sys.time()
+  nonCoverageFreqArray <- computeNonCoverageFreqArray(
+    superReplicationCount = mySuperReplicationCount,
+    replicationCount = myReplicationCount,
+    sampleSize = mySampleSize,
+    lag = myLag,
+    tParArray = myTParArray,
+    kernel = myKernel,
+    bandwidth = myBandwidth,
+    nonCoverageProbability = myNonCoverageProbability
+  )
+  End=Sys.time()
+  Duration=End-Start
+
+  cat("nonCoverageArray= ",nonCoverageFreqArray)
+  cat("\nDuration= ",Duration)
+
+}
+
+test_that("Testing \'computeNonCoverageFreqArray\'", {
+  computeNonCoverageFreqArrayFunction()
+  cat("\nEnd of test computeNonCoverageFreqArray", "\n")
+  cat("=====================")
+})
