9d8af085b54789072694dc5ec9deeb7562f47803
Gograman <gograman93@gmail.com>
2017-09-20 11:56:34 +0300

    rename test

---
+++ SCB/tests/testthat/tests_09_computeTermCount.R
@@ -0,0 +1,11 @@
+computeTermCountFunction <- function()
+{
+
+}
+
+test_that("Testing \'computeTermCount\'", {
+  computeTermCountFunction()
+  cat("\n End of test computeTermCount", "\n")
+  cat("=====================")
+})
+
---
+++ SCB/tests/testthat/tests_11_isInputCompatible.R
@@ -0,0 +1,11 @@
+isInputCompatibleFunction <- function()
+{
+
+}
+
+test_that("Testing \'isInputCompatible\'", {
+  isInputCompatibleFunction()
+  cat("\n End of test isInputCompatible", "\n")
+  cat("=====================")
+})
+
