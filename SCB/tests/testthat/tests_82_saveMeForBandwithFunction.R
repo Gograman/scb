ad3cffe20451facc392577d3f03095c465f45140
Adlet <botahanov@aol.com>
2017-09-28 19:00:45 +0600

    new graphics ME

---
+++ SCB/tests/testthat/tests_82_saveMeForBandwithFunction.R
@@ -0,0 +1,65 @@
+saveMeForBandwithFunction <- function()
+{
+  workingDirectory<-getwd()
+  splitDirectory <- data.frame(strsplit(workingDirectory,"/"))
+  tailDirectory <- tail(splitDirectory,1)
+
+  myPath <- "../../../util/meWithBreakedBandwidth.R"
+
+  if(tailDirectory=="SCB")
+  {
+    myPath <-"../util/meWithBreakedBandwidth.R"
+  }
+  source(myPath)
+  sampleSize <- 500
+  bandwith <- seq(0.1,0.9,0.2)
+  tParCount <- 10
+  tParArray <- createTParArray(tParCount)
+
+  nonCoverageProbability <- 0.2
+  lag <- 1
+  errorIfNotInputCompatible(alpha = nonCoverageProbability,lag = lag,sampleSize = sampleSize)
+  lagCount <- computeLagCount(sampleSize = sampleSize,lag = lag)
+  sample <- createSample(sampleSize)
+  meArray <- matrix(0,nrow = length(tParArray),ncol = length(bandwith))
+  for(index in 1:length(bandwith))
+  {
+    tempBandwith <- bandwith[index]
+    corArray <- computeAllCorHatsForTest(tParArray = tParArray,
+                                         lagCount = lagCount,
+                                         sample = sample,
+                                         bandwidth = tempBandwith)
+    meArray[,index] <- computeMEForTest(tParArray = tParArray,
+                                lag = lag,
+                                lagCount = lagCount,
+                                sample = sample,
+                                bandwidth = tempBandwith,
+                                nonCoverageProbability = nonCoverageProbability,
+                                allCorHats = corArray)
+  }
+  fileName <- paste("MEforBandWith","Alpha",nonCoverageProbability,"SS",sampleSize,sep = "_")
+  xlab <- "TparArray"
+  subTitle <- paste(xlab,
+    "\tParCount= ", tParCount,
+    ", Alpha = ", nonCoverageProbability,
+    ", lag = " , lag,
+    ", sampleS = ", sampleSize,
+    "\nAlpha, sample,SampleSize fixed",
+    sep = "")
+
+  path <- doPath()
+  saveCVS(fileName = fileName,path = path,dataToSave = meArray)
+  saveJpg(fileName = fileName,path = path)
+  matplot(tParArray,meArray,type = "l",col = 1:length(bandwith),xlab = "")
+  title(main = "ME vs SampleSize, fixed alpha and T",sub = subTitle)
+  legend("topright",title = "bandwith",legend = bandwith,col = 1:length(bandwith))
+  graphics.off()
+}
+test_that("testing saveMeForBandwithFunction",{
+
+
+  saveMeForBandwithFunction()
+  cat(" End of test saveMeForBandwithFunction", "\n")
+  cat("=========================================")
+})
+
