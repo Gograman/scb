743e6a5850a1f31bd195937c2e9d18b2016ce55e
Gograman <gograman93@gmail.com>
2017-10-10 15:15:40 +0300

    batch file

--- SCB/tests/testthat/run.bat
+++ SCB/tests/testthat/run.bat
@@ -1,4 +1,4 @@
-set path=%path%;"C:\Program Files\R\R-3.4.1\bin"
+set path=%path%;"C:\Program Files\R\R-3.4.2\bin"
 path
 
-"C:\Program Files\R\R-3.4.1\bin\R.exe" CMD BATCH --vanilla --slave "myScript.R"
+"C:\Program Files\R\R-3.4.2\bin\R.exe" library(devtools)
