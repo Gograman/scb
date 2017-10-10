81066125503ef3a9f745e6a4073e5e17d5ae991d
statistics101 <garbuzov.boris@gmail.com>
2017-10-10 10:01:52 -0400

    added comments

--- SCB/R/getDegree.R
+++ SCB/R/getDegree.R
@@ -1,5 +1,12 @@
+
+# Must be between -0.5 and -0.11111
+# bandwidth increases when degree increases
+# If I make degree=-0.12, the automatic sampleSize=300 approximately,
+# and testthat runs slowly.
+# If I make degree=-0.4, the automatic sampleSize=6 approximately,
+# and testthat runs fast.
 getDegree <- function()
 {
-  degree <- -0.3
+  degree <- -0.4
   return(degree)
 }
