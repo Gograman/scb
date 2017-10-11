b9a5345d2f6c8f28de22bb1ef1db3ae7b47371f2
statistics101 <garbuzov.boris@gmail.com>
2017-10-11 10:15:38 -0400

    bug directory

---
+++ bugs/betaLRVOutOfBound.R
@@ -0,0 +1,20 @@
+# chain
+# lab/BugsReplicationCount.R
+# testUtilReplicationCountBenchmark.R
+# computeNonCoverageFreqArray.R
+# comuteNonCoverageFrequency.R
+# createBandsBrick.R
+# createBand.R
+# computeMEByCovHat.R
+# computeBetaLRVHat.R
+
+# systematize on which formulas do we calculate all sizes
+# size of allCorHat by tParCount and by lagCount+1
+# which function?
+# all procedures that calculate the sizes
+# Perhaps termCount is larger than lagCount + lag + 1
+# simplify the environment
+# shorten the chain
+
+
+
