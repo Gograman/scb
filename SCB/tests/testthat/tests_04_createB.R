836fc9855b2bd2e94e124520e77ec1e937c1d792
Gograman <gograman93@gmail.com>
2017-10-29 12:44:30 +0200

    changes

--- SCB/tests/testthat/tests_04_createB.R
+++ SCB/tests/testthat/tests_04_createB.R
@@ -1,26 +1,11 @@
-createBFunction<-function(sampleSize=5)
-{
-  cat("\n Testing \'tests_04_createB\' \n")
-  lag <- 1
-  alpha <- 0.05
-  mySampleSize = computeMinSampleSize(alpha = alpha)
-  errorIfNotInputCompatible(alpha = alpha,lag = lag,mySampleSize)
-  Start=Sys.time()
-  b <- computeB(mySampleSize)
-  End=Sys.time()
-  Duration=End-Start
+  test_that(desc = "Testing createB",
+            code = {
+              testUtilCreateB(sampleSize = 5)
+            })
 
-  cat("bandwidth =", b, "\n")
-  cat("Duration =",Duration,"\n")
-  cat("=====================")
-  cat("\nTest parameters :","\n")
-  cat("sampleSize= ",sampleSize,"\n")
-}
-  test_that("Testing  \'createB\'", {
-    createBFunction()
-    cat(" End of test createB","\n")
-    cat("=====================")
-  }
-  )
+
+
+
+
 
 
--- SCB/tests/testthat/tests_06_errorIfNotInputCompatible.R
+++ SCB/tests/testthat/tests_06_errorIfNotInputCompatible.R
@@ -1,15 +1,4 @@
-errorIfNotInputCompatibleFunction=function()
-{
-  cat("\n Testing \'tests_06_errorIfNotInputCompatible\'")
 
-  myLag= 1
-  myAlpha <- 0.3
-  mySampleSize <- computeMinSampleSize(myAlpha)
-  errorIfNotInputCompatible(alpha = myAlpha,sampleSize = mySampleSize,lag=myLag)
-
-}
 test_that("Testing \'errorIfNotInputCompatibleFunction\'", {
-  errorIfNotInputCompatibleFunction()
-  cat("\n End of test errorIfNotInputCompatible", "\n")
-  cat("=====================")
+testUtilErrorIfNotInputCompatible()
 })
